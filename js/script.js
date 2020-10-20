6c5db6d8e4b2bdec029bc86d06f6f29e61854ef3
Alex <ZimenkovIUAI@gmail.com>
2020-10-20 21:02:23 +0300

    add mobile version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 	$('.count__last').text($('.slider--upper .slider__item').length);
-
 	$('.slider--lower').slick({
 			arrows: false,
 			speed: 1000,
 			infinite: false,
 			draggable: false,
-			sNavFor: '.slider--upper'
+			swipe: false,
+			sNavFor: '.slider--upper',
+			responsive:[
+				{
+					breakpoint: 768,
+					settings:{
+						arrows: true,
+						swipe: true
+					}
+				}
+			],
 		}
 	);
 	$('.slider--upper').slick({
@@ -16,15 +25,25 @@ $(document).ready(function(){
 			slidesToScroll: 1,
 			variableWidth: true,
 			draggable: false,
+			swipe: false,
+			focusOnSelect:true,
 			asNavFor: '.slider--lower'
 		}
 	);
 	$('.slider--upper').on('beforeChange', function(event, slick, currentSlider, nextSlide){
-		$('.count__first').text(nextSlide+1);
+		count(nextSlide, '.count__first', '.count__last');
+	});	
+	$('.slider--lower').on('beforeChange', function(event, slick, currentSlider, nextSlide){
+		count(nextSlide, '.count__first', '.count__last');
+	});	
+}); 
+
+function count(nextSlide, current, total) {
+	$(current).text(nextSlide+1);
+	$(total).text($('.slider--upper .slider__item').length);
 		if(nextSlide+1 == $('.slider--upper .slider__item').length){
 			$('.slick-next').addClass('last-slide');
 		} else if ($('.slick-next').hasClass('last-slide')){
 			$('.slick-next').removeClass('last-slide');
 		}
-	});	
-}); 
+}
